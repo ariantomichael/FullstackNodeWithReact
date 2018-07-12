0251009e287090e349a6250cd3848193f8dedbba
Mike <mike.ak1412@gmail.com>
2018-07-12 15:58:34 +0900

    test 2nd deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hey: 'boy' });
+  res.send({ hey: PORT });
 });
 
 const PORT = process.env.PORT || 5000;
