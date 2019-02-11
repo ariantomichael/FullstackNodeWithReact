4faae9702fc85353adc6c5f75cc7cb6a5899a729
Mike <mike.ak1412@gmail.com>
2019-02-12 00:20:17 +0900

    add dev.js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+      '773987466490-2ho7avg14lnrq73mvs7hh47kjn6fmmu6.apps.googleusercontent.com',
+    googleClientSecret: 'xfbGbk_2lrGW30ykQmXn-yHR',
+    mongoURI: 'mongodb://mike:password1@ds018568.mlab.com:18568/emaily-dev',
+    cookieKey: 'awihjuhfwauhwodijaoiwjdoaijwdaowidj'
+  };
